ff5870cac54ef5aa8852b67f388c7c902705f1f9
Gurkan Akcakaya <gurkan.akcakaya@cegeka.com>
2023-07-06 20:20:40 +0200

    FEAT - app.js homestack

--- events-app/App.js
+++ events-app/App.js
@@ -1,13 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import 'react-native-gesture-handler';
+import { NavigationContainer } from "@react-navigation/native";
+import { HomeStack } from './navigation/stack'
 
 // React Native web  corresponding tags
 // https://reactnative.dev/docs/intro-react-native-components#core-components
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text>Hello World</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+    <HomeStack></HomeStack>
+    </NavigationContainer>
   );
 }
